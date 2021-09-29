aa9916a11709fa50b723e2b92e311f64e152275e
Kankana Bordoloi <kankana.bordoloi@gmail.com>
2021-09-29 16:45:02 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -77,7 +77,7 @@ def _capture_screenshot():
     return driver.get_screenshot_as_base64()
 
 
-@pytest.fixture(params=[os.environ.get("CI")], scope="function")
+@pytest.fixture(params=[os.environ.get("CI")], scope="class")
 def init_driver(request):
     settings = load_settings()
     chrome_options = Options()
