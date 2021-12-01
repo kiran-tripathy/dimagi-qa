0d01ad58890ebb6c4d6901d5a4968d7f9a4d0a07
kankana <kankana.bordoloi@gmail.com>
2021-12-01 14:32:22 +0530

    updated conftest

--- HQSmokeTests/testCases/conftest.py
+++ HQSmokeTests/testCases/conftest.py
@@ -63,7 +63,8 @@ def settings(environment_settings):
 
 @pytest.fixture(scope="session")
 def driver(request, settings):
-    print(os.environ.get('DISPLAY'))
+    
+    print("DISPLAY:",os.environ.get('DISPLAY'))
     chrome_options = webdriver.ChromeOptions()
     # xvfb = Xvfb(width=1280, height=720)
     # xvfb.start()
@@ -72,11 +73,12 @@ def driver(request, settings):
         chrome_options.add_argument('disable-extensions')
         chrome_options.add_argument('--safebrowsing-disable-download-protection')
         chrome_options.add_argument('--safebrowsing-disable-extension-blacklist')
-        chrome_options.add_argument('window-size=1920,1080')
+        # chrome_options.add_argument('window-size=1920,1080')
+        chrome_options.add_argument("--disable-setuid-sandbox")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--remote-debugging-port=9222')
+        chrome_options.add_argument('--remote-debugging-port=0')
         chrome_options.add_argument('--headless')
         chrome_options.add_experimental_option("prefs", {
             "download.default_directory": str(UserInputsData.download_path),
