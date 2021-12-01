12f141ba19a7eb98651a43985ce3c221e6811476
kankana <kankana.bordoloi@gmail.com>
2021-12-01 16:28:34 +0530

    updated email_conf

--- HQSmokeTests/utilities/email_conf.py
+++ HQSmokeTests/utilities/email_conf.py
@@ -1,12 +1,12 @@
 # imap host and email details
 imaphost = "imap.gmail.com"
-username = "kbordoloi@dimagi.com"
+username = ${{secrets.DIMAGI_MAIL_USERNAME}}
 
 # using app password due to 2FA
-app_password = "ydfxtggbtggodgrn"
+app_password = ${{secrets.DIMAGI_MAIL_PASSWORD}}
 
 # details for sending the email
 smtp_ssl_host = 'smtp.gmail.com'
 smtp_ssl_port = 465
-sender = 'kbordoloi@dimagi.com'
-targets = ['kbordoloi@dimagi.com','ktripathy@dimagi.com','btalbot@dimagi.com']
+sender = ${{secrets.DIMAGI_MAIL_USERNAME}}
+targets = ['kbordoloi@dimagi.com','qa@dimagi.com']
