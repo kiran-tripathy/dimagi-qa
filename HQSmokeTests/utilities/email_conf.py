591597feb25bb0999675c6366dc0f745515aa634
kankana <kankana.bordoloi@gmail.com>
2021-12-01 16:56:08 +0530

    updated email_conf and yml

--- HQSmokeTests/utilities/email_conf.py
+++ HQSmokeTests/utilities/email_conf.py
@@ -1,14 +1,12 @@
-from HQSmokeTests.testCases.conftest import settings
-
 # imap host and email details
 imaphost = "imap.gmail.com"
-username = settings['mail_username']
+username = "kbordoloi@dimagi.com"
 
 # using app password due to 2FA
-app_password = settings['mail_password']
+app_password = "ydfxtggbtggodgrn"
 
 # details for sending the email
 smtp_ssl_host = 'smtp.gmail.com'
 smtp_ssl_port = 465
-sender = settings['mail_username']
+sender = 'kbordoloi@dimagi.com'
 targets = ['kbordoloi@dimagi.com','qa@dimagi.com']
