59afb57f765efe04f05405acdf76f744292ebc6c
kankana <kankana.bordoloi@gmail.com>
2021-12-01 14:14:58 +0530

    updated conftest

--- HQSmokeTests/testCases/conftest.py
+++ HQSmokeTests/testCases/conftest.py
@@ -75,6 +75,7 @@ def driver(request, settings):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--start-maximized')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--remote-debugging-port=9222')
         chrome_options.add_argument('--headless')
         chrome_options.add_experimental_option("prefs", {
             "download.default_directory": str(UserInputsData.download_path),
