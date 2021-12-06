db3b13d1aaa8f1255a897c08d7adb03010a08eef
kankana <kankana.bordoloi@gmail.com>
2021-12-06 14:29:29 +0530

    made changes to email_conf and email_pytest_report to incorporate env var and updated yml with logs path

--- HQSmokeTests/utilities/email_conf.py
+++ HQSmokeTests/utilities/email_conf.py
@@ -1,24 +1,12 @@
-def email_conf(settings):
-    email_dict = {
-        "imaphost" : "imap.gmail.com",
-        "username": settings["login_username"],
-        "app_password": settings["login_password"],
-        "smtp_ssl_host" : "smtp.gmail.com",
-        "smtp_ssl_port" : "465",
-        "sender" : settings["login_username"],
-        "targets" : ['kbordoloi@dimagi.com']
-    }
-    return (email_dict)
+# imap host and email details
+imaphost = "imap.gmail.com"
+username = settings["mail_username"]
 
-# # imap host and email details
-# imaphost = "imap.gmail.com"
-# username = "kbordoloi@dimagi.com"
-#
-# # using app password due to 2FA
-# app_password = "ydfxtggbtggodgrn"
-#
-# # details for sending the email
-# smtp_ssl_host = 'smtp.gmail.com'
-# smtp_ssl_port = 465
-# sender = "kbordoloi@dimagi.com"
-# targets = ['kbordoloi@dimagi.com','qa@dimagi.com']
+# using app password due to 2FA
+app_password = settings["mail_password"]
+
+# details for sending the email
+smtp_ssl_host = 'smtp.gmail.com'
+smtp_ssl_port = 465
+sender = settings["mail_username"]
+targets = ['kbordoloi@dimagi.com','qa@dimagi.com']
